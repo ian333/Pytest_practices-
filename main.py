298fce20ebe3420de4a33b8914adbb37d38eaff5
Ian <sebastianvaz123@hotmail.com>
2022-01-06 00:00:28 -0600

    Cambios al gitignore y primeros tests
    Signed-off-by: Ian <sebastianvaz123@hotmail.com>

---
+++ main.py
@@ -0,0 +1,9 @@
+def suma(x: float ,y : float) -> float:
+    
+    """Funcion que genera toma 2 argumentos y devuelve una suma 
+    
+    args:
+        x:(float): primer valor a sumar 
+        y:(float): segundo valor a sumar 
+    """
+    return x+y
